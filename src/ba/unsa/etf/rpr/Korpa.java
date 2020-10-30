ca916ba9d51d4fb362c5d1ff00c50fed3502a32d
mborovac1 <mborovac1@etf.unsa.ba>
2020-10-30 18:11:23 +0100

    Implementirana metoda izbaciArtiklSaKodom u klasama Supermarket i Korpa

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -20,4 +20,23 @@ public class Korpa {
         return artikli;
     }
 
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        Artikl pomocni = new Artikl("pomocni", 1, "pomocni");
+
+        for(int i = 0; i < brojArtikala; i++) {
+            if(artikli[i].getKod().equals(kod)) {
+                pomocni = artikli[i];
+                artikli[i] = null;
+                for(int j = i; j < brojArtikala - 1; j++) {
+                    artikli[j] = artikli[j + 1];
+                }
+                brojArtikala = brojArtikala - 1;
+                // ne mora i-- jer piše u tekstu zadatka da su svi kodovi artikala različiti ... možemo odma break petlju
+                break;
+            }
+        }
+
+        return pomocni;
+    }
+
 }
