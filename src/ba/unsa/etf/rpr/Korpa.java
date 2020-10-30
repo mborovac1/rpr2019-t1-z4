52a2753aaeca53e82d974d273ca9eea604062c9e
mborovac1 <mborovac1@etf.unsa.ba>
2020-10-30 18:07:07 +0100

    Implementirani getteri i metoda dodajArtikl u klasama Supermarket i Korpa

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,23 @@
 package ba.unsa.etf.rpr;
 
 public class Korpa {
+    private Artikl[] artikli = new Artikl[50];
+    private static int brojArtikala = 0;
+
+    public boolean dodajArtikl(Artikl a) {
+        if(brojArtikala > 49) {
+            // throw new Exception("Dostignut maksimalan broj artikala u supermarketu");
+            return false;
+        }
+        else {
+            artikli[brojArtikala] = a;
+            brojArtikala = brojArtikala + 1;
+            return true;
+        }
+    }
+
+    public Artikl[] getArtikli() {
+        return artikli;
+    }
+
 }
